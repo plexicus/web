f40f8d28dca2d6f52490c5f74c70c152d811de13
luky <luzion1508@gmail.com>
2025-04-28 09:03:46 +0700

    [update] eslint & stylistic config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 export default {
 	rules: {
 		// override/add rules settings here, such as:
-		'@stylistic/ts/indent': ['error', 'tab']
+		// '@stylistic/ts/indent': ['error', 'tab']
 		// "astro/no-set-html-directive": "error"
 	}
-};
+}
