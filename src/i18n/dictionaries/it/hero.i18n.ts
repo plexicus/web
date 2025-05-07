86352e5768ea8f9ab41cc4a201c8f4b13af7117c
luky <luzion1508@gmail.com>
2025-05-07 20:20:14 +0700

    [feat] remove unused typescript dictionary files

--- src/i18n/dictionaries/it/hero.i18n.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  'hero.subtitle': 'Il nostro ASPM è progettato per essere il tuo Agente AI di Cybersecurity, offrendo supporto continuo, guida e soluzioni automatizzate per mantenere sicura la tua catena di fornitura del software contro minacce in evoluzione.',
-  'hero.title': 'Agente AI\nRemediatore di\nVulnerabilità',
-  'hero.get_started': 'Inizia',
-  'hero.learn_more': 'Scopri di più',
-};
