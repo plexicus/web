15e3723a649601e920da93cedddefcee8789ba50
Irvine Pramudya <irvinepramudya@gmail.com>
2025-05-12 12:09:45 +0700

    [feature] add site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,2 @@
 export const SITE_TITLE = 'Plexicus - AI Agent Vulnerability Remediator';
+export const SITE_DESCRIPTION = 'Our ASPM is designed to be your cybersecurity AI Agent, offering continuous support, guidance, and automated solutions to keep your software supply chain secure against evolving threats';
