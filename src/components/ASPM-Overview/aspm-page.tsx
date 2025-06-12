6a69562f12a3846d822070875c644cadeb04d26c
Slackluky <luzion1508@gmail.com>
2025-06-12 19:24:32 +0700

    [rework] Add Security Co-Pilot component to ASPM overview page layout

--- src/components/ASPM-Overview/aspm-page.tsx
+++ src/components/ASPM-Overview/aspm-page.tsx
@@ -9,6 +9,7 @@ import ComplianceAutomation from '@/components/ASPM-Overview/compliance-automati
 import RealWorldImpact from '@/components/ASPM-Overview/real-world-impact';
 import CTASection from '../CTA/section';
 import GetStarted from './get-started';
+import InteractiveAspmSection from './security-co-pilot';
 export default function AspmPage({ lang }) {
   return (
     <main className="min-h-screen bg-white">
@@ -16,8 +17,9 @@ export default function AspmPage({ lang }) {
       <TechnicalProblem lang={lang} />
       <CoreCapabilities lang={lang} />
       <AiRemediation lang={lang} />
+      <InteractiveAspmSection />
       <IntegrationEcosystem lang={lang} />
-      <DevSecOpsIntegration lang={lang} />
+      { /* <DevSecOpsIntegration lang={lang} /> */ }
       <ComplianceAutomation lang={lang} />
       <RealWorldImpact lang={lang} />
       <GetStarted lang={lang} />
